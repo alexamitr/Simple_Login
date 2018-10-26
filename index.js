312b6aa2999dbed91c03a03b19820bb17176d90e
alexamitr <alexa.mitronika@gmail.com>
2018-10-26 09:05:26 +0100

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// Wrap every letter in a span
+$('.fr1').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.fr1 .letter',
+      scale: [4,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 950,
+      delay: function(el, i) {
+        return 70*i;
+      }
+    }).add({
+      targets: '.fr1',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
